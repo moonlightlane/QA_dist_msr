5e11d5f94c8a235c15a64e020af47a4152528a1b
moonlightlane <wangzichao6@gmail.com>
2017-10-02 09:57:34 -0500

    init commit

---
+++ OpenNMT-py/onmt/modules/__init__.py
@@ -0,0 +1,22 @@
+from onmt.modules.UtilClass import LayerNorm, Bottle, BottleLinear, \
+    BottleLayerNorm, BottleSoftmax, Elementwise
+from onmt.modules.Gate import ContextGateFactory
+from onmt.modules.GlobalAttention import GlobalAttention
+from onmt.modules.ConvMultiStepAttention import ConvMultiStepAttention
+from onmt.modules.ImageEncoder import ImageEncoder
+from onmt.modules.CopyGenerator import CopyGenerator, CopyCriterion
+from onmt.modules.StructuredAttention import MatrixTree
+from onmt.modules.Transformer import TransformerEncoder, TransformerDecoder
+from onmt.modules.Conv2Conv import CNNEncoder, CNNDecoder
+from onmt.modules.MultiHeadedAttn import MultiHeadedAttention
+from onmt.modules.StackedRNN import StackedLSTM, StackedGRU
+from onmt.modules.Embeddings import Embeddings
+from onmt.modules.WeightNorm import WeightNormConv2d
+
+
+# For flake8 compatibility.
+__all__ = [GlobalAttention, ImageEncoder, CopyGenerator, MultiHeadedAttention,
+           LayerNorm, Bottle, BottleLinear, BottleLayerNorm, BottleSoftmax,
+           TransformerEncoder, TransformerDecoder, Embeddings, Elementwise,
+           CopyCriterion, MatrixTree, WeightNormConv2d, ConvMultiStepAttention,
+           CNNEncoder, CNNDecoder, StackedLSTM, StackedGRU, ContextGateFactory]
